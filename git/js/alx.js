3994e57ecfc0990e0461d6461873b5241e38f70a
Aggie49 <atienookongo@gmail.com>
2023-07-07 23:48:05 -0700

    Starting to code today, so cool

---
+++ git/js/alx.js
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+console.log(ALX)
+=======
+console.log("ALX")
+>>>>>>> 09afbfa0ab3e62da72427ecf2781d7098710c175
---
+++ git/js/school.js
@@ -0,0 +1,5 @@
+console.log(School)
+<<<<<<< HEAD
+=======
+
+>>>>>>> 09afbfa0ab3e62da72427ecf2781d7098710c175
